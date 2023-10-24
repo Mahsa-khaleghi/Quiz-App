c816a9c6868e94c8c1fcb413bed8b299e26b2bbb
Mahsa Khaleghi <mahsaa.khaleghi20@gmail.com>
2023-10-24 10:00:48 +0330

    complete local storage

--- js/script.js
+++ js/script.js
@@ -2,52 +2,77 @@ import quizData from "./questions.js";
 
 const quiz = document.querySelector(".quiz-box");
 const inpAnswers = document.querySelectorAll(".answer");
-const lable = document.querySelectorAll(".ans-lable");
 const question = document.querySelector(".question");
 const result = document.getElementById("result");
 const score = document.getElementById("score");
 
 // Answers
-// استفاده نشده
-// const [ans_1,ans_2,ans_3,ans_4] = document.querySelectorAll("ans_1,ans_2,ans_3,ans_4");
 const ans_1 = document.getElementById("ans_1");
 const ans_2 = document.getElementById("ans_2");
 const ans_3 = document.getElementById("ans_3");
 const ans_4 = document.getElementById("ans_4");
 
 // Buttons
-
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const subBtn = document.querySelector(".submit");
+const reloadBtn = document.querySelector(".reload");
 
-let currentQuiz = 0;
 let answered = 0;
+let clickBack = 0;
+const randomQuiz = 100;
 let answerSelected = {};
+let ranNums = [];
+let currentQuiz = 0;
 
-loadQuiz();
-//متن سوال و جواب را می گیرد
+//refresh
+window.onbeforeunload = function (e) {
+  localStorage.clear();
+};
+
 function loadQuiz() {
-  const currentQuizData = quizData[currentQuiz];
-  question.innerText = currentQuizData.question;
-  ans_1.innerText = currentQuizData.answer_1;
-  ans_2.innerText = currentQuizData.answer_2;
-  ans_3.innerText = currentQuizData.answer_3;
-  ans_4.innerText = currentQuizData.answer_4;
+  if (clickBack == 0) {
+    currentQuiz = Math.floor(Math.random() * quizData.length) + 1;
+    let counter = 0;
+    while (0 < ranNums.length < 10 && ranNums.includes(currentQuiz)) {
+      counter++;
+      currentQuiz = Math.floor(Math.random() * quizData.length) + 1;
+      if (counter === 20) {
+        break;
+      }
+    }
+    ranNums.push(currentQuiz);
+  } else {
+    clickBack = 1;
+    console.log(ranNums);
+    currentQuiz = ranNums[ranNums.length - 1];
+  }
+  const currentQuizData = quizData[currentQuiz - 1];
+  question.innerText = currentQuizData ? currentQuizData.question : "";
+
+  if (currentQuizData) {
+    ans_1.innerText = currentQuizData.answer_1;
+    ans_2.innerText = currentQuizData.answer_2;
+    ans_3.innerText = currentQuizData.answer_3;
+    ans_4.innerText = currentQuizData.answer_4;
+  }
   deSelectAnswer();
 
-  // پاسخ کاربر نشان داده می شود
+  // در ابتدا دکمه برگشت نشان داده نشود
+  prevBtn.style.display = "none";
 
-  if (answerSelected[currentQuiz]) {
-    let selected = answerSelected[currentQuiz];
-    document.getElementById(selected).checked = true;
-  }
   // اگر سوالی برای نمایش وجود نداشته باشد دکمه تایید ظاهر شود
-  if (currentQuiz == quizData.length - 1) {
+  let allQuestion = JSON.parse(localStorage.getItem("question"));
+  let lastQuestion = 0;
+  if (allQuestion && Array.isArray(allQuestion)) {
+    lastQuestion = allQuestion.length;
+  }
+  if (lastQuestion == quizData.length - 1) {
     nextBtn.style.display = "none";
     subBtn.style.display = "block";
   }
 }
+loadQuiz();
 
 // جواب از حالت انتخاب خارج شود
 
@@ -60,15 +85,23 @@ function deSelectAnswer() {
 //به سوال بعدی می رود
 
 nextBtn.addEventListener("click", () => {
+  if (quizData.length == 0) {
+    reloadBtn.style.display = "block";
+    localStorage.clear();
+  }
+  addToStorage(quizData[currentQuiz - 1] ? quizData[currentQuiz - 1].id : null);
   let answer = getSelected();
-  console.log(answer, quizData[currentQuiz].correct);
   if (answer) {
-    if (answer == quizData[currentQuiz].correct) {
+    if (
+      answer ===
+      (quizData[currentQuiz - 1] ? quizData[currentQuiz - 1].correct : null)
+    ) {
       answered++;
     }
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
+    if (currentQuiz <= quizData.length && getSelected()) {
       loadQuiz();
+      // دکمه برگشت نمایش داده شود
+      prevBtn.style.display = "block";
     }
   }
 });
@@ -76,36 +109,78 @@ nextBtn.addEventListener("click", () => {
 // به سوال قبلی می رود
 
 prevBtn.addEventListener("click", () => {
-  if (currentQuiz > 0) {
-    currentQuiz--;
+  clickBack = 1;
+  if (ranNums.length > 0) {
+    //delete local storage
+    let allQuestions = JSON.parse(localStorage.getItem("question"));
+    allQuestions.splice(allQuestions.indexOf(currentQuiz), 1);
+    localStorage.setItem("question", JSON.stringify(allQuestions));
+
+    //delete ranNum
+    const index = ranNums.indexOf(randomQuiz);
+    const remove = ranNums.splice(index, 1);
     loadQuiz();
+    console.log(`myArray values: ${ranNums}`);
+    console.log("remove:", remove);
+
+    prevBtn.style.display = "block";
+    nextBtn.style.display = "block";
+    subBtn.style.display = "none";
+  }
+  if (ranNums.length == 1) {
+    clickBack = 0;
+    prevBtn.style.display = "none";
   }
 });
 
 // دکمه تایید (submit button)
-
 subBtn.addEventListener("click", () => {
-  if (getSelected()) {
+  addToStorage(quizData[currentQuiz - 1] ? quizData[currentQuiz - 1].id : null);
+  if (getSelected) {
     quiz.style.display = "none";
     result.style.display = "block";
-
+    reloadBtn.style.display = "block";
     let answer = getSelected();
-    if (answer == quizData[currentQuiz].correct) {
+    if (
+      answer == quizData[currentQuiz - 1]
+        ? quizData[currentQuiz - 1].correct
+        : null
+    ) {
       answered++;
     }
-    score.innerText = answered + "/" + quizData.length + " " + "questions answer correctly";
+    score.innerText =
+      answered + "/" + quizData.length + " " + "questions answer correctly";
   }
 });
 
 // بررسی می کندآیا یک چک باکس علامت زده شده است یا خیر
-
 function getSelected() {
   let answer;
   inpAnswers.forEach((inpAnswer, index) => {
     if (inpAnswer.checked) {
+      answer = inpAnswer.id;
+      answerSelected[currentQuiz] = answer;
       answer = "answer_" + (index + 1);
-      // answerSelected[currentQuiz] = answer;
     }
   });
+
   return answer;
 }
+
+let getOldQuestions = () => {
+  let allQuestions = JSON.parse(localStorage.getItem("question"));
+  if (allQuestions) {
+    return allQuestions;
+  }
+  return [];
+};
+
+let addToStorage = (newQuestion) => {
+  let oldQuestions = getOldQuestions();
+  let allQuestions = JSON.parse(localStorage.getItem("question"));
+  if (Array.isArray(allQuestions) && allQuestions.includes(newQuestion)) {
+  } else {
+    oldQuestions.push(newQuestion);
+    localStorage.setItem("question", JSON.stringify(oldQuestions));
+  }
+};
